594748f8ff1c3231648752502860b41b9eed63d2
ducduy2312 <ducduy2312@gmail.com>
2017-06-22 13:02:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-#test edit
 
 from __future__ import print_function
 from future.standard_library import install_aliases
@@ -11,6 +10,7 @@ from urllib.error import HTTPError
 
 import json
 import os
+import re
 
 from flask import Flask
 from flask import request
@@ -19,6 +19,66 @@ from flask import make_response
 # Flask app should start in global layout
 app = Flask(__name__)
 
+#dictionary for translation
+en-vi = {
+'0':'vòi rồng',
+'1':'bão nhiệt đới',
+'2':'bão lớn',
+'3':'dông lớn kèm sấm sét',
+'4':'dông kèm sấm sét',
+'5':'mưa kèm với tuyết',
+'6':'mưa và mưa tuyết',
+'7':'tuyết và mưa tuyết',
+'8':'mưa phùn kèm theo hơi lạnh',
+'9':'mưa phùn',
+'10':'mưa kèm theo hơi lạnh',
+'11':'có mưa',
+'12':'có mưa',
+'13':'tuyết phủ',
+'14':'tuyết rơi nhẹ',
+'15':'nhiều tuyết ',
+'16':'có tuyết',
+'17':'mưa đá',
+'18':'mưa tuyết',
+'19':'bụi mù',
+'20':'có sương',
+'21':'bao bọc sương mù',
+'22':'sương mù',
+'23':'gió thổi mạnh',
+'24':'có gió',
+'25':'trời lạnh',
+'26':'có mây',
+'27':'nhiều mây',
+'28':'nhiều mây',
+'29':'có mây bao phủ',
+'30':'có mây bao phủ',
+'31':'trời trong',
+'32':'trời nắng đẹp',
+'33':'trời đẹp',
+'34':'trời đẹp',
+'35':'mưa kèm mưa đá',
+'36':'trời nóng',
+'37':'có thể có dông',
+'38':'có thể có dông bao phủ',
+'39':'có thể có dông bao phủ',
+'40':'có thể có mưa',
+'41':'tuyết lớn',
+'42':'có thể có tuyết rơi xuống',
+'43':'tuyết lớn',
+'44':'trời có mây nhẹ',
+'45':'mưa kèm sớm chớp',
+'46':'tuyết rơi nhiều',
+'47':'có thể có mưa kèm sớm chớp',
+'3200':'thời tiết chưa cập nhật',
+'Hanoi':'Hà Nội',
+'hanoi':'Hà Nội',
+'Ho Chi Minh':'Hồ Chí Minh',
+}
+
+vi-en = {
+
+
+}
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
@@ -43,7 +103,7 @@ def processRequest(req):
     yql_query = makeYqlQuery(req)
     if yql_query is None:
         return {}
-    yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
+    yql_url = baseurl + urlencode({'q': yql_query}) + "&u=c&format=json"
     result = urlopen(yql_url).read()
     data = json.loads(result)
     res = makeWebhookResult(data)
@@ -83,10 +143,12 @@ def makeWebhookResult(data):
     if condition is None:
         return {}
 
+	pattern = re.compile('|'.join(en-vi.keys()))
+	
     # print(json.dumps(item, indent=4))
 
-    speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
-             ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
+    speech = "Hôm nay ở " + location.get('city') + ": " + pattern.sub(lambda x: d[x.group()], condition.get('code')) + \
+             ", nhiệt độ trung bình là " + condition.get('temp') + " độ" 
 
     print("Response:")
     print(speech)
@@ -102,7 +164,7 @@ def makeWebhookResult(data):
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
-
+	
     print("Starting app on port %d" % port)
 
     app.run(debug=False, port=port, host='0.0.0.0')
